cfd018b37c5758aae9939c3b8105d6d6f6394a36
nkibrislioglu <56323444+nkibrislioglu@users.noreply.github.com>
2019-11-30 15:03:28 -0500

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Step 1 in Capstone Project
+# Writing some python codes and sharing it in github
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
